8bb2b69492b5544c03f7f51c0e130c4ad0c3a59d
libaojie <libaojie@hirisun.com>
2021-01-29 18:52:39 +0800

    db

--- python_wheel/lbj_db/poetry.bat
+++ python_wheel/lbj_db/poetry.bat
@@ -2,8 +2,17 @@
 REM 声明采用UTF-8编码
 chcp 65001
 
-echo 初始化环境
+:: 区分是否有环境
+for /f "delims=" %%i in ('python -m poetry env info -p') do set env_path=%%i
+
+if "%env_path%"=="" (
+echo 无环境
 python -m poetry install
+) else (
+echo 有环境
+python -m poetry run python -m pip install --upgrade pip
+)
+
 echo 更新环境
 python -m poetry update
 echo setuptools降级
