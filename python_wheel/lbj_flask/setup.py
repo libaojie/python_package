94eb8f4f6dfdb27c75a9111d2a63db26ee20ea5b
libaojie <libaojie@qq.com>
2022-09-20 18:15:41 +0800

    flask版本升级

--- python_wheel/lbj_flask/setup.py
+++ python_wheel/lbj_flask/setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='lbj-flask',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     install_requires=[
         "gevent==1.4.0",
         "gunicorn==19.9.0",
         "requests==2.18.4",
-        "flask==0.12.1",
-        "flask-restful==0.3.6",
+        "flask==2.0.3",
+        "flask-restful==0.3.9",
         "flask-sqlalchemy==2.3.0",
         "flask-cors==3.0.4",
-        "Werkzeug==0.15"
+        "Werkzeug==2.0"
     ],
     description='Flask公共插件',
     url='None',
