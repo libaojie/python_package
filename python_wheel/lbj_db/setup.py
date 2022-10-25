866bc081d264c01517dd74afbd87f69184d35214
libaojie <leebaojie@gmail.com>
2022-10-25 18:37:06 +0800

    lbj-db 由python3.7.4升级为3.8.8

--- python_wheel/lbj_db/setup.py
+++ python_wheel/lbj_db/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lbj-db',
-    version='1.0',
+    version='2.0',
     packages=find_packages(),
     install_requires=[
         "sqlalchemy ==1.2.0",
