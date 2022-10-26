b0308f915d62be2a98fb085427bf26a0492e34a0
libaojie <leebaojie@gmail.com>
2022-10-26 16:13:20 +0800

    04_flask

---
+++ python_template/04_flask/poetry.bat
@@ -0,0 +1,21 @@
+@ echo off
+REM 声明采用UTF-8编码
+chcp 65001
+
+echo 初始化环境
+python -m poetry install
+echo 更新环境
+python -m poetry update
+REM echo setuptools降级
+REM python -m poetry run pip install setuptools==19.2.0
+
+echo ------------------------------------------------
+python -m poetry show
+echo ------------------------------------------------
+python -m poetry show --tree
+echo ------------------------------------------------
+
+
+python -m poetry env info -p | clip
+
+pause
