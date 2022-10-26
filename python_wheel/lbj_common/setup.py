d0e4537d2182ef2dd944c18b35ccdd6b7522feb5
libaojie <leebaojie@gmail.com>
2022-10-26 15:32:27 +0800

    lbj_common

--- python_wheel/lbj_common/setup.py
+++ python_wheel/lbj_common/setup.py
@@ -6,8 +6,8 @@ setup(
     version='2.0',
     packages=find_packages(),
     install_requires=[
-        "setuptools==49.2.1",
-        "wheel==0.34.2"
+        "setuptools>=49.2.1",
+        "wheel>=0.34.2"
     ],
     description='基础工具包',
     url='None',
